656e22592ee608a9d4eca9d0472e80d558e0f626
JTabach <jorget300@outlook.com>
2021-12-03 23:13:21 -0300

    Commit: Desafio Funciones

---
+++ js/clase.js
@@ -0,0 +1,13 @@
+// casa de cambi
+
+//usuario debe ingresar cantidad de pesos a cambiar
+
+const valorDolar = 840;
+let pesosACambiar = parseInt(prompt("Cuántos pesos desea cambiar?"));
+let resultado = pesosACambiar/valorDolar
+
+console.log("Pesos a cambiar "+pesosACambiar);
+console.log("Dolares recibidos "+resultado);
+
+
+function 
